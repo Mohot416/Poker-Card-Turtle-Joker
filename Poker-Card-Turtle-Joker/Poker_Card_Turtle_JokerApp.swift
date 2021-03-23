edc0855e143c943694d1221185d70014dd264143
mj0323 <jackjemmy72@gmail.com>
2021-03-23 18:35:29 +0800

    Initial Commit

---
+++ Poker-Card-Turtle-Joker/Poker_Card_Turtle_JokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poker_Card_Turtle_JokerApp.swift
+//  Poker-Card-Turtle-Joker
+//
+//  Created by User05 on 2021/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Poker_Card_Turtle_JokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
